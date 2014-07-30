d3ddeeafc3cf6b0b8ea9a017fb937d5e8f85f4c9
lyr90329 <lyr90329@163.com>
2014-07-30 15:55:23 +0800

    Create DB Util Class

---
+++ src/com/lyr/util/DBConnection.java
@@ -0,0 +1,88 @@
+package com.lyr.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ * Database util class
+ * 
+ * @author luyanran
+ * 
+ */
+public class DBConnection {
+	private static final String USERNAME = "root";
+	private static final String PASSWORD = "sdp123";
+	private static final String URL = "jdbc:mysql://localhost:3306/db_affairmanage";
+	private static final String DRIVER = "com.mysql.jdbc.Driver";
+
+	/**
+	 * Get Connection
+	 * 
+	 * @return
+	 */
+	public static Connection getConnection() {
+		Connection conn = null;
+		try {
+			Class.forName(DRIVER);
+			conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return conn;
+	}
+
+	/**
+	 * Close Connection
+	 * 
+	 * @param conn
+	 */
+	public static void close(Connection conn) {
+		if (conn != null) {
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
+	/**
+	 * Close PreparedStatement
+	 * 
+	 * @param pstmt
+	 */
+	public static void close(PreparedStatement pstmt) {
+		if (pstmt != null) {
+			try {
+				pstmt.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
+	/**
+	 * Close ResultSet
+	 * 
+	 * @param rs
+	 */
+	public static void close(ResultSet rs) {
+		if (rs != null) {
+			try {
+				rs.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+}
