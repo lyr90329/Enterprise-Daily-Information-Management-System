b3bcba1420cd804366adc4e8983dddbb94d8bf8c
lyr90329 <lyr90329@163.com>
2014-07-30 17:39:13 +0800

    Reply and Criticism Bean

---
+++ src/com/lyr/bean/Reply.java
@@ -0,0 +1,52 @@
+package com.lyr.bean;
+
+import java.util.Date;
+
+public class Reply {
+	private int replyID;
+	private String replyContent;
+	private int employeeID;
+	private Date replyTime;
+	private int messageID;
+
+	public int getReplyID() {
+		return replyID;
+	}
+
+	public void setReplyID(int replyID) {
+		this.replyID = replyID;
+	}
+
+	public String getReplyContent() {
+		return replyContent;
+	}
+
+	public void setReplyContent(String replyContent) {
+		this.replyContent = replyContent;
+	}
+
+	public int getEmployeeID() {
+		return employeeID;
+	}
+
+	public void setEmployeeID(int employeeID) {
+		this.employeeID = employeeID;
+	}
+
+	public Date getReplyTime() {
+		return replyTime;
+	}
+
+	public void setReplyTime(Date replyTime) {
+		this.replyTime = replyTime;
+	}
+
+	public int getMessageID() {
+		return messageID;
+	}
+
+	public void setMessageID(int messageID) {
+		this.messageID = messageID;
+	}
+
+}
